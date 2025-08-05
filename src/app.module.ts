5b59ed672584d00c19848601ed52385253714b83
Muhammad Hamza Saeed <151459755+HamzaSaeed704@users.noreply.github.com>
2025-08-06 01:48:13 +0500

    Add files via upload

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+// src/app.module.ts
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'hamza123',
+      database: 'wallet',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UserModule,
+  ],
+})
+export class AppModule {}
